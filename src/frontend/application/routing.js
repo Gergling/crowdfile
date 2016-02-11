68682519e6e946c215495fe60c73a196de91f86e
Greg <gdavies@internationalfx.com>
2016-02-11 14:44:09 +0000

    Basic routing principles.

--- src/frontend/application/routing.js
+++ src/frontend/application/routing.js
@@ -1,5 +1,13 @@
 import angular from 'angular';
 
-angular.module('application').config(() => {
-  // Various state-ui routing
+angular.module('application').config(($stateProvider) => {
+  $stateProvider.state('dashboard', {
+      url: '/',
+      templateUrl: 'dashboard/index.html',
+      controller: 'dashboardController'
+    })
+    .state('404', {
+      url: '/*path',
+      templateUrl: 'application/404.html'
+    });
 });
